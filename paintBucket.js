1e04f30473355bed865433bf67dc695a7d1159b2
lcyin <laichunyinkevin@gmail.com>
2018-10-25 00:28:45 +0800

    not all pixel replace, color pixel cannot show properly

--- paintBucket.js
+++ paintBucket.js
@@ -1,103 +1,135 @@
-class PaintBucket extends PaintFunction{
-    constructor(contextReal){
+class PaintBucket extends PaintFunction {
+    constructor(contextReal) {
         super();
         this.context = contextReal;
         this.pixelStack = [];
-        this.colorLayer;
+        this.imgData;
         this.curColor = {
-            r: 203,
-            g: 53,
-            b: 148
-        }           
+            r: 191,
+            g: 63,
+            b: 63
+        };
     }
-    
-    onMouseDown(coord){
-        this.colorLayer = this.context.getImageData(0, 0, canvasReal.width, canvasReal.height)
 
-        console.log(this.colorLayer)
+    onMouseDown(coord) {
+
+        this.imgData = this.context.getImageData(0, 0, canvasReal.width, canvasReal.height)
+        console.log(this.imgData.data)
+        this.floodFill(coord)
+        console.log(this.imgData.data)
+        console.log(this.context)
+        var cur = this.context.putImageData(this.imgData, 0, 0);
+        console.log(cur)
+
+
+
+    }
+    onDragging(coord) {}
+
+    onMouseMove() {}
+    onMouseUp() {}
+    onMouseLeave() {}
+    onMouseEnter() {}
+    floodFill(coord){
+        var newPos,
+            x,
+            y,
+            pixelPos,
+            reachLeft,
+            reachRight,
+            boundTop = 0,
+            boundLeft = 0,
+            boundRight = canvasReal.width - 1,
+            boundBottom = canvasReal.width - 1
+
+
+        // console.log(this.imgData)
         this.pixelStack.push(coord)
-        console.log(this.pixelStack)
-        while(this.pixelStack.length)
-        {
-            var newPos, x, y, pixelPos, reachLeft, reachRight;
+        console.log(this.pixelStack[0])
+        while (this.pixelStack.length) {
+
+
             newPos = this.pixelStack.pop();
             x = newPos[0];
             y = newPos[1];
 
-            pixelPos = (y*canvasReal.width + x) *4;
-            while (y-- >= 0 && this.matchStartColor(pixelPos)){
-                pixelPos = canvasReal.width * 4;
+            //get current pixel position
+            pixelPos = (y * canvasReal.width + x) * 4;
+
+
+            //go up as long as the color matches and are inside the canvas
+            while (y >= 0 && this.matchStartColor(pixelPos)) {
+                y -= 1;
+                pixelPos -= canvasReal.width * 4;
             }
+
+
             pixelPos += canvasReal.width * 4;
-            ++y;
+            y += 1;
             reachLeft = false;
             reachRight = false;
-            while(y++ < canvasReal.height-1 && this.matchStartColor(pixelPos))
-            {
-                colorPixel(pixelPos);
 
-                if(x > 0)
-                {
-                if(matchStartColor(pixelPos - 4))
-                {
-                    if(!reachLeft){
-                    this.pixelStack.push([x - 1, y]);
-                    reachLeft = true;
+            //go down as long as the color matches in inside the canvas
+            while (y <= canvasReal.height - 1 && this.matchStartColor(pixelPos)) {
+                y += 1;
+                this.colorPixel(pixelPos);
+
+                if (x > 0) {
+                    if (this.matchStartColor(pixelPos - 4)) {
+                        if (!reachLeft) {
+                            //add pixel to stack
+                            this.pixelStack.push([x - 1, y])
+                            //prevent adding pixel that will eventually handled by the downward march of the pixel we just add
+                            reachLeft = true;
+                        }
+
+                    } else if (reachLeft) {
+                        reachLeft = false;
                     }
                 }
-                else if(reachLeft)
-                {
-                    reachLeft = false;
-                }
-                }
-                
-                if(x < canvasReal.width-1)
-                {
-                if(this.matchStartColor(pixelPos + 4))
-                {
-                    if(!reachRight)
-                    {
-                    pixelStack.push([x + 1, y]);
-                    reachRight = true;
+
+                //look at right
+                if (x < canvasReal.width - 1) {
+                    if (this.matchStartColor(pixelPos + 4)) {
+                        if (!reachRight) {
+                            //add pixel to stack
+                            this.pixelStack.push(x + 1, y);
+                            reachRight = true;
+                        }
+                    } else if (reachRight) {
+                        reachRight = false;
                     }
+
                 }
-                else if(reachRight)
-                {
-                    reachRight = false;
-                }
-                }
-                        
                 pixelPos += canvasReal.width * 4;
             }
         }
-        this.context.putImageData(this.colorLayer, 0, 0);
-            
+    }
+    //check curPixel equal to starPixel
+    matchStartColor(pixelPos, startR, startG, startB) {
+        startR = 0
+        startG = 0
+        startB = 0
+
+        var r = this.imgData.data[pixelPos];
+        var g = this.imgData.data[pixelPos + 1];
+        var b = this.imgData.data[pixelPos + 2];
+        //   console.log(r,g,b)
+        //   console.log(startR, startG, startB)
+        //   console.log((r == startR && g == startG && b == startB));
+
+        return (r !== startR && g !== startG && b !== startB);
 
     }
-    onDragging(coord){
-        this.draw(coord[0],coord[1]);
-    }
 
-    onMouseMove(){}
-    onMouseUp(){}
-    onMouseLeave(){}
-    onMouseEnter(){}
+    colorPixel(pixelPos) {
+        
+        this.imgData.data[pixelPos] = this.curColor.r;
+        this.imgData.data[pixelPos + 1] = this.curColor.g;
+        this.imgData.data[pixelPos + 2] = this.curColor.b;
+        this.imgData.data[pixelPos + 3] = 1;
+        //   console.log('fill')
+        //   console.log(this.imgData.data[pixelPos],this.imgData.data[pixelPos + 1],this.imgData.data[pixelPos +2],this.imgData.data[pixelPos+3])
 
-  
-    matchStartColor(pixelPos, startR, startG, startB)
-    {
-      var r = this.colorLayer.data[pixelPos];	
-      var g = this.colorLayer.data[pixelPos+1];	
-      var b = this.colorLayer.data[pixelPos+2];
-    
-      return (r == startR && g == startG && b == startB);
-    }
-    
-    colorPixel(pixelPos)
-    {
-      this.colorLayer.data[pixelPos] = this.curColor.r;
-      this.colorLayer.data[pixelPos+1] = this.curColor.g;
-      this.colorLayer.data[pixelPos+2] = this.curColor.b;
-      this.colorLayer.data[pixelPos+3] = 255;
     }
 }
