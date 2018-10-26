d2d33c7cba0fcdc1a32b4388793de09154b2cb6d
lcyin <laichunyinkevin@gmail.com>
2018-10-26 10:14:08 +0800

    kevin commit

--- drawing-polygon.js
+++ drawing-polygon.js
@@ -3,44 +3,37 @@ class DrawingPolygon extends PaintFunction {
         super();
         this.contextReal = contextReal;
         this.contextDraft = contextDraft;
-        // this.contextDraft.strokeStyle = '#bbbbbb'; //william modify 
-        // this.contextDraft.fillStyle = 'white'; //william modify 
         this.contextDraft.lineJoin = 'round';
         this.contextDraft.lineWidth = 5;
-        // this.contextReal.strokeStyle = '#bbbbbb'; //william modify
-        // this.contextReal.fillStyle = '#aaaaaa'; //william modify
         this.contextReal.lineJoin = 'round';
         this.contextReal.lineWidth = 5;
         this.polygonCoord = [];
         this.down = false;
-
-        this.finish = $('#finish').click(function(e){
-            //terminate mousemove
-            currentFunction.down=false;
-            if(currentFunction.polygonCoord.length > 0){
-                currentFunction.finishPolygon(currentFunction.contextReal,currentFunction.polygonCoord);
-                currentFunction.contextDraft.clearRect(0,0,canvasReal.width,canvasReal.height);
-                //clear polygonCoord  
-                currentFunction.polygonCoord = [];
-            }
-        })
     }
 
     onMouseDown(coord,event){
 
         this.origX = coord[0];
         this.origY = coord[1];
+        //record polygon coord
         this.polygonCoord.push({upX: this.origX,upY:this.origY})
+        //prevent draft 
         this.down = true;
+        //set dash or solid line
         this.isDash(this.contextReal);
+        //if coord exsist loop over each coord
         if(this.polygonCoord.length > 1){
         for (var x=1; x<this.polygonCoord.length;x++){
             this.contextReal.beginPath();
+            //draw line according to each coord
             this.contextReal.moveTo(this.polygonCoord[x-1].upX,this.polygonCoord[x-1].upY);
             this.contextReal.lineTo(this.polygonCoord[x].upX,this.polygonCoord[x].upY);
             this.contextReal.stroke();
             }
         }
+        //finsh draw polygon
+        this.isFinish()
+        //set collor
         this.contextReal.fillStyle = document.getElementById("color").value; //william modify
         this.contextDraft.fillStyle = document.getElementById("color").value; // william modify
         this.contextReal.strokeStyle = document.getElementById("color").value; //william modify
@@ -51,17 +44,21 @@ class DrawingPolygon extends PaintFunction {
     }
 
     onMouseMove(coord, event){
-        this.moveX = coord[0];
-        this.moveY = coord[1];
+        //clear draft
         this.contextDraft.clearRect(0,0,canvasReal.width,canvasReal.height);
+            //prevent draft
             if (this.down == true) {
+                //draft polygon
                 this.contextDraft.beginPath();
                 this.contextDraft.moveTo(this.origX,this.origY);
                 this.contextDraft.lineTo(coord[0], coord[1]);
                 this.contextDraft.stroke();
-
-                for (var x=0;x<this.polygonCoord.length;x++){
-                    this.contextDraft.lineTo(this.polygonCoord[x].upX,this.polygonCoord[x].upY)                
+                //draft fill
+                if(selected.FILL == 1){
+                    for (var x=0;x<this.polygonCoord.length;x++){
+                        this.contextDraft.lineTo(this.polygonCoord[x].upX,this.polygonCoord[x].upY)                
+                    }
+                    this.contextDraft.fill()
                 }
             }
     };
@@ -79,14 +76,25 @@ class DrawingPolygon extends PaintFunction {
             ctx.setLineDash([])
         }
     }
+    //click button to finish
+    isFinish(){
+        $('#finish').click(function(e){
+            //stop mousemove
+            currentFunction.down=false;
+            if(currentFunction.polygonCoord.length > 0){
+                currentFunction.finishPolygon(currentFunction.contextReal,currentFunction.polygonCoord);
+                currentFunction.contextDraft.clearRect(0,0,canvasReal.width,canvasReal.height);
+                //clear polygonCoord  
+                currentFunction.polygonCoord = [];
+            }
+        })
+
+    }
     finishPolygon(ctx, array){
         ctx.moveTo(array[0].upX,array[0].upY)
         for (var i=1;i<array.length;i++){
             ctx.lineTo(array[i].upX,array[i].upY);
         }
-        
-
-
         if (selected.FILL==0){
             ctx.beginPath();
             ctx.moveTo(array[array.length-1].upX,array[array.length-1].upY);
@@ -98,6 +106,5 @@ class DrawingPolygon extends PaintFunction {
         }
     }
     
- 
 
 }
--- paintBucket.js
+++ paintBucket.js
@@ -13,13 +13,17 @@ class PaintBucket extends PaintFunction {
 
     onMouseDown(coord) {
         this.HEXtoRGB()
+        //get start image data(array of pixel)
         this.imgData = this.context.getImageData(0, 0, canvasReal.width, canvasReal.height)
+        //set start pixel location
         this.startPOS = (coord[1] * canvasReal.width + coord[0]) * 4;
+        //set start rgb color
         this.startR = this.imgData.data[this.startPOS];
         this.startG = this.imgData.data[this.startPOS + 1];
         this.startB = this.imgData.data[this.startPOS + 2];
+        //flood fill
         this.floodFill(coord)
-        //put filled image
+        //update image
         this.context.putImageData(this.imgData, 0, 0);
     }
     onDragging(coord) {}
@@ -35,44 +39,52 @@ class PaintBucket extends PaintFunction {
             pixelPos,
             reachLeft,
             reachRight
-
+        //add task to pixel stack
         this.pixelStack.push(coord)
 
         while (this.pixelStack.length) {
+            //set new locaiton from pixel stack
             newPos = this.pixelStack.pop();
             x = newPos[0];
             y = newPos[1];
 
-            //get current pixel position
+            //set new pixel location
             pixelPos = (y * canvasReal.width + x) * 4;
 
-            //go up as long as the color matches and are inside the canvas
+            //go up as long as the color matches and are inside the canvas (return true)
             while (y >= 0 && this.matchStartColor(pixelPos)) {
                 y -= 1;
                 pixelPos -= canvasReal.width * 4;
             }
+            //update
             pixelPos += canvasReal.width * 4;
             y += 1;
+
+            //initialize for addtion of pixel stack
             reachLeft = false;
             reachRight = false;
 
             //go down as long as the color matches in inside the canvas
             while (y <= canvasReal.height - 1 && this.matchStartColor(pixelPos)) {
-                y += 1;
+                //fill current pixel location
                 this.colorPixel(pixelPos);
+                //downward
+                y += 1;
 
                 //check left pixel
                 if (x > 0) {
+                    //match start color return true
                     if (this.matchStartColor(pixelPos - 4)) {
-
+                        //if not yet reach left edge 
                         if (!reachLeft) {
-                            //add pixel to stack
+                            //add pixel to stack to handle it later
                             this.pixelStack.push([x - 1, y])
                             //prevent adding pixel that will eventually handled by the downward march of the pixel we just add
                             reachLeft = true;
                         }
 
                     } else if (reachLeft) {
+                        //color not match then change reach Left to false, for adding next pixel stack
                         reachLeft = false;
                     }
                 }
@@ -81,7 +93,6 @@ class PaintBucket extends PaintFunction {
                 if (x < canvasReal.width - 1) {
                     if (this.matchStartColor(pixelPos + 4)) {
                         if (!reachRight) {
-                            //add pixel to stack
                             this.pixelStack.push([x + 1, y]);
                             reachRight = true;
                         }
@@ -90,20 +101,21 @@ class PaintBucket extends PaintFunction {
                     }
 
                 }
+                //should be last valid pixel location
                 pixelPos += canvasReal.width * 4;
             }
 
         }
 
     }
-    //check curPixel equal to starPixel
+    //check new color equal to start color
     matchStartColor(pixelPos) {
         var r = this.imgData.data[pixelPos];
         var g = this.imgData.data[pixelPos + 1];
         var b = this.imgData.data[pixelPos + 2];
         return (r === this.startR && g === this.startG && b === this.startB);
     }
-    //fill current color
+    //fill color
     colorPixel(pixelPos) {
         this.imgData.data[pixelPos] = this.curColor.r;
         this.imgData.data[pixelPos + 1] = this.curColor.g;
@@ -114,22 +126,28 @@ class PaintBucket extends PaintFunction {
     HEXtoRGB() {
         var hex = document.getElementById("color").value;
         if (hex.charAt(0) === '#') {
+            //remove #
             hex = hex.substr(1);
         }
-        if ((hex.length < 2) || (hex.length > 6)) {
+        //valid hex color
+        if (hex.length > 6) {
             return false;
         }
+        //split to array of 6
         var values = hex.split(''),
             r,
             g,
             b;
         if (hex.length === 6) {
+            //parse to rgb color (16 = hexadecimel)
+            //each rgb occupy 2 hex number
             r = parseInt(values[0].toString() + values[1].toString(), 16);
             g = parseInt(values[2].toString() + values[3].toString(), 16);
             b = parseInt(values[4].toString() + values[5].toString(), 16);
         } else {
             return false;
         }
+        //set current rgb color
         return this.curColor = {
             r,
             g,
